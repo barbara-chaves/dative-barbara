8878835798037e7e675c03f6188b42fff6984a32
barbara-chaves <barbarabarcellos90@gmail.com>
2019-11-06 22:46:35 +0100

    create docs

---
+++ docs/precache-manifest.45419714fb3a19f46b03f7a9c2c7c603.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92ab2899c223d8ba75f1b6ff4d67fbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62eccc507c5585b8328b",
+    "url": "/static/css/main.00c6076b.chunk.css"
+  },
+  {
+    "revision": "8261aa88e4a11c104a53",
+    "url": "/static/js/2.b3d9447a.chunk.js"
+  },
+  {
+    "revision": "62eccc507c5585b8328b",
+    "url": "/static/js/main.3f71818f.chunk.js"
+  },
+  {
+    "revision": "b7896837217ec3ae4ac9",
+    "url": "/static/js/runtime-main.7141ac6c.js"
+  },
+  {
+    "revision": "b18da40b019287e852ce614f55468934",
+    "url": "/static/media/dative-logo.b18da40b.webp"
+  },
+  {
+    "revision": "ebb44511e4b768be161f92ad85460f14",
+    "url": "/static/media/earthquake-logo.ebb44511.png"
+  }
+]);
